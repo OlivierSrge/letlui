4adf1cbe751bcbab23945eb1019e70fbdc95b5ae
OlivierSrge <olivierfinestone@gmail.com>
2026-04-24 11:51:35 +0100

    Fix: Mise à jour URL Apps Script Version 9

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
   // URL de votre Google Apps Script déployé en Web App
   // (voir README.md pour les instructions)
-  API_URL: "https://script.google.com/macros/s/AKfycbw4DLfc2prDXAgVoGCaa47lSDma0yvpeNjNgZ4KJdeBDlDXpSqGS7pjR2njj8DY10yYMg/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbzYBfoPN5pYJf74bJt8s9avZNAeR-tO56K2YYdzJ9FCQZpnTPQ_AQ2pgzApSaYFiMaKOg/exec",
 
   // Nom de votre boutique
   NOM_BOUTIQUE: "L et Lui Signature",
