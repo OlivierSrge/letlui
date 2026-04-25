6160b42f16d8c81186624d7b386afa3e99328377
OlivierSrge <olivierfinestone@gmail.com>
2026-04-25 13:41:13 +0100

    UPDATE: Nouvelle URL Apps Script V17 - Pass VIP webhook + Images

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
   // URL de votre Google Apps Script déployé en Web App
   // (voir README.md pour les instructions)
-  API_URL: "https://script.google.com/macros/s/AKfycbzYBfoPN5pYJf74bJt8s9avZNAeR-tO56K2YYdzJ9FCQZpnTPQ_AQ2pgzApSaYFiMaKOg/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbyhE1fw4V3yHHcOf_NWvp6hhZ3co-SmrC0B8tVcH1cH_a7dMJfHPsEkmEXvcLoHmsgStg/exec",
 
   // Nom de votre boutique
   NOM_BOUTIQUE: "L et Lui Signature",
