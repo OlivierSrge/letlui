c8abbf7b60c47516d2bc89786175ad51ee53e7e6
OlivierSrge <olivierfinestone@gmail.com>
2026-04-25 20:12:11 +0100

    fix: URL Apps Script Version 20 - Email HTML Pass VIP

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
   // URL de votre Google Apps Script déployé en Web App
   // (voir README.md pour les instructions)
-  API_URL: "https://script.google.com/macros/s/AKfycbyhE1fw4V3yHHcOf_NWvp6hhZ3co-SmrC0B8tVcH1cH_a7dMJfHPsEkmEXvcLoHmsgStg/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbyAwJBVH8CTz0EERLsQiDUKGYEJMYrHQ4i48vkVaBrwNj3GCB9gye9fTaPw8IfD4Z5x/exec",
 
   // Nom de votre boutique
   NOM_BOUTIQUE: "L et Lui Signature",
